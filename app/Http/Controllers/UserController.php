9845590b68bf5ada077d63bd0f2449283144c039
happer64bit <happer64bit@gmail.com>
2026-02-24 13:18:39 +0630

    add user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function getUserById(Request $request, $id)
+    {}
+
+    public function updateSelfUserInfo(Request $request)
+    {}
+}
