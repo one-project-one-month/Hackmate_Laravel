2d97bf72126c5c007260ef78aa125bdcf2f2b583
Thantzinaung94 <thantzinaung2025.94@gmail.com>
2026-03-05 01:01:30 +0630

    feat: implement get other user's public info

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function getUserById(Request $request, $id)
-    {}
+    {
+        $user = User::find($id);
+        if(!$user){
+            return response()->json([
+                'success' => false,
+                'message' => 'User not found',
+                'status' => 404
+            ], 404);
+        }
+
+        return response()->json([
+            'success' => true,
+            'message' => 'successful',
+            'content' => $user,
+            'status' => 200
+        ]);
+    }
 
     public function updateSelfUserInfo(Request $request)
     {}
