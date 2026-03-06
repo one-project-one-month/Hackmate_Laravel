73adba67812730b8f8f401f97ab8cbe72d71c4cc
happer64bit <happer64bit@gmail.com>
2026-03-06 21:23:01 +0630

    Fix API auth error handling and request import

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
