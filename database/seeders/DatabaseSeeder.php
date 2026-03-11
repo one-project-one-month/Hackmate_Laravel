ca787965081b13259678074f7221802328de793b
happer64bit <happer64bit@gmail.com>
2026-03-11 21:19:29 +0630

    add more tech stacks

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            LoginTestUserSeeder::class,
-            ProjectSeeder::class,       //  run project seeder
+            TechStackSeeder::class,
+            LoginTestUserSeeder::class
         ]);
     }
 }
