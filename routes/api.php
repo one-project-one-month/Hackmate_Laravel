7eb2137096186b9753d8c704ddd93e9f2889e2e7
wintkhantlin <happer64bit@gmail.com>
2026-03-27 12:35:33 +0630

    add register route

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::prefix('v1')->group(function () {
     Route::get('/users/{id}', [UserController::class, 'getUserById']);
 
     Route::prefix('auth')->group(function () {
+        Route::post('/register', [AuthController::class, 'register']);
         Route::post('/login', [AuthController::class, 'login']);
         Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
         Route::post('/reset-password', [AuthController::class, 'resetPassword']);
